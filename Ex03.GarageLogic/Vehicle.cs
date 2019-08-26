4573b1b21d7b4d19f64c02fbb0acea7a4a2782eb
edKantor <edankantor94@gmail.com>
2019-08-27 00:33:52 +0300

    starting classes

---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex03.GarageLogic
+{
+    public abstract class Vehicle
+    {
+        private readonly string r_LicencePlateNumber;
+        private readonly string r_Model;
+        private float m_EnergyPercentage;
+        Wheel[] m_Wheels;
+
+
+    }
+}
---
+++ Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex03.GarageLogic
+{
+    // TBD: class access modifier 
+    public class Wheel
+    {
+        private readonly string r_Manufactor;
+        private float m_CurrentTirePressure;
+        private float m_MaxTirePressure;
+
+        public void Inflate(float i_PressureToAdd)
+        {
+
+        }
+
+
+    }
+}
