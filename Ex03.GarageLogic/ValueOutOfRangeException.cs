afdf8305c134caef431581993d08fd7c1e2981b7
edan <edan@atomation.net>
2019-08-29 01:05:29 +0300

    ArgumentWrapper added ValueOutOfRangeException added

--- Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -8,7 +8,49 @@ namespace Ex03.GarageLogic
 {
     class ValueOutOfRangeException : Exception
     {
-        float m_MaxValue;
-        float m_MinValue;
+        private readonly Nullable<float> r_MaxValue;
+        private readonly Nullable<float> r_MinValue;
+        
+
+        public ValueOutOfRangeException(Nullable<float> i_MaxValue, Nullable<float> i_MinValue)
+        {
+            r_MaxValue = i_MaxValue;
+            r_MinValue = i_MinValue;
+        }
+
+        public Nullable<float> MaxValue
+        {
+            get
+            {
+                return r_MaxValue;
+            }
+        }
+
+        public Nullable<float> MinValue
+        {
+            get
+            {
+                return r_MinValue;
+            }
+        }
+
+        public override string Message
+        {
+            get
+            {
+                StringBuilder exceptionMessageStringBuilder = new StringBuilder();
+                exceptionMessageStringBuilder.Append("The value was ");
+                if(MinValue != null)
+                {
+                    exceptionMessageStringBuilder.AppendFormat("over the {0} limit ", MaxValue);
+                }
+                else
+                {
+                    exceptionMessageStringBuilder.AppendFormat("under the {0} limit ", MinValue);
+                }
+
+                return exceptionMessageStringBuilder.ToString();
+            }
+        }
     }
 }
