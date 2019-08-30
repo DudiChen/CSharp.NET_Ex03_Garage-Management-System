3e15e6dec1a77c7b846c22ba946a6e1b4c43f764
edan <edan@atomation.net>
2019-08-31 02:08:58 +0300

    corrected wheelPressure

--- Ex03.GarageLogic/Wheel.cs
+++ Ex03.GarageLogic/Wheel.cs
@@ -61,7 +61,7 @@ namespace Ex03.GarageLogic
 
         public override string ToString()
         {
-            return string.Format(@"Manufacturer name: {0}   Max tire pressure: {1}  Current tire pressure: {2}", r_Manufacturer, r_MaxTirePressure, m_CurrentTirePressure);
+            return string.Format(@"Manufacturer name: {0}   Current tire pressure: {1}", r_Manufacturer, m_CurrentTirePressure);
         }
     }
 }
